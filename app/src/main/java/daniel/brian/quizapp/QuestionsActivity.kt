fae16d8f2440876ae94dcba9723d9ad46cce819b
Daniel Brian <db9755949@gmail.com>
2023-06-19 00:56:02 +0300

    Second UI Screen

--- app/src/main/java/daniel/brian/quizapp/QuestionsActivity.kt
+++ app/src/main/java/daniel/brian/quizapp/QuestionsActivity.kt
@@ -3,8 +3,21 @@ package daniel.brian.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuestionsActivity : AppCompatActivity() {
+    private lateinit var progressBar : ProgressBar
+    private lateinit var tv_progress : TextView
+    private lateinit var tv_question : TextView
+    private lateinit var iv_image : ImageView
+    private lateinit var optionOne : TextView
+    private lateinit var optionTwo : TextView
+    private lateinit var optionThree : TextView
+    private lateinit var optionFour : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
@@ -12,6 +25,28 @@ class QuestionsActivity : AppCompatActivity() {
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size","${questionsList.size}")
 
+        progressBar = findViewById(R.id.progressBar)
+        tv_progress = findViewById(R.id.tv_progress)
+        tv_question = findViewById(R.id.tv_question)
+        iv_image = findViewById(R.id.iv_image)
+        optionOne = findViewById(R.id.optionOne)
+        optionTwo = findViewById(R.id.optionTwo)
+        optionThree = findViewById(R.id.optionThree)
+        optionFour = findViewById(R.id.optionFour)
+
+
+        val currentPosition = 1
+        val question : Questions? = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.Question
+        iv_image.setImageResource(question.image)
+        optionOne.text = question.optionOne
+        optionTwo.text = question.optionTwo
+        optionThree.text = question.optionThree
+        optionFour.text = question.optionFour
 
     }
 }
