33029dfe76895c1e1bcf0ccb6ce89ec6b77db0a6
Daniel Brian <db9755949@gmail.com>
2023-06-20 23:49:07 +0300

    ADDED: The Button Functionality to Questions Activity

--- app/src/main/java/daniel/brian/quizapp/QuestionsActivity.kt
+++ app/src/main/java/daniel/brian/quizapp/QuestionsActivity.kt
@@ -1,13 +1,17 @@
 package daniel.brian.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var progressBar : ProgressBar
     private lateinit var tv_progress : TextView
     private lateinit var tv_question : TextView
@@ -18,13 +22,21 @@ class QuestionsActivity : AppCompatActivity() {
     private lateinit var optionFour : TextView
 
 
+    private var mCurrentPosition : Int = 1
+    private var mQuestionList : ArrayList<Questions>? = null
+    private var mSelectedOptionPosition : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size","${questionsList.size}")
+        // To access the buttons
+        optionOne.setOnClickListener(this)
+        optionTwo.setOnClickListener(this)
+        optionThree.setOnClickListener(this)
+        optionFour.setOnClickListener(this)
 
+        // Finding elements buy Id
         progressBar = findViewById(R.id.progressBar)
         tv_progress = findViewById(R.id.tv_progress)
         tv_question = findViewById(R.id.tv_question)
@@ -34,12 +46,18 @@ class QuestionsActivity : AppCompatActivity() {
         optionThree = findViewById(R.id.optionThree)
         optionFour = findViewById(R.id.optionFour)
 
+        mQuestionList = Constants.getQuestions()
+        setQuestion()
 
-        val currentPosition = 1
-        val question : Questions? = questionsList[currentPosition - 1]
+    }
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+    private fun setQuestion () {
+
+        mCurrentPosition = 1
+        val question = mQuestionList!![mCurrentPosition  - 1]
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition " + "/" + progressBar.max
 
         tv_question.text = question!!.Question
         iv_image.setImageResource(question.image)
@@ -48,5 +66,45 @@ class QuestionsActivity : AppCompatActivity() {
         optionThree.text = question.optionThree
         optionFour.text = question.optionFour
 
+        // This will be set the default after every question
+        defaultOptionView()
+
+    }
+
+    private fun defaultOptionView () {
+
+        val options = ArrayList<TextView>()
+
+        options.add(0,optionOne)
+        options.add(1,optionOne)
+        options.add(2,optionOne)
+        options.add(3,optionOne)
+
+        for(option in options){
+
+            // This code set the default background
+
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,
+                R.drawable.default_option_border_bg)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+    private fun selectedOptionView (tv: TextView, selectedOptionNum : Int){
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this,
+            R.drawable.selcected_option_border_bg)
+
+        defaultOptionView()
     }
 }
